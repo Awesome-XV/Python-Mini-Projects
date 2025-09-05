592c6e32537a535493cf294890b74901d7788cb3
Awesome-XV <147341789+Awesome-XV@users.noreply.github.com>
2025-09-05 18:45:57 -0500

    Added reaction mini game inside terminal game

--- Iron-shelter/COYA.py
+++ Iron-shelter/COYA.py
@@ -1,8 +1,12 @@
 import sys
+from plans import missile_building
+import time
+import random
+import msvcrt
 
 def main():
-    print("Well hello there you have been found in 5747 and he been asleep for some amout of years we don't know but we need your help.")
-    print("There was a nuclear fallout and half of the world is destoryed and we need your knowledge to make the nukes again or at least a few missiles.")
+    print("Well hello there you have been found in 5747 and have been asleep for some amount of years we don't know but we need your help.")
+    print("There was a nuclear fallout and half of the world has been destroyed and we need your knowledge to make the nukes again or at least a few missiles.")
 
     result = input("Do you accept this task? Yes or No")
 
@@ -22,10 +26,10 @@ def main():
 
     if (result == "Yes") or (result == "yes"):
         print("You're in the field and your first task is to navigate to our research center and you have been given a weapon (Gun).")
-        result = input("You have reached a enemy force of 2 soldiers in the way to you confront them?")
+        result = input("You have reached an enemy force of 2 soldiers in the way. Do you confront them?")
     else:
-        print("you have been found in 5747 and he been asleep for some amout of years we don't know but we need your help.")
-        print("There was a nuclear fallout and half of the world is destoryed and we need your knowledge to make the nukes again or at least a few missiles.")
+        print("You have been found in 5747 and have been asleep for some amount of years we don't know but we need your help.")
+        print("There was a nuclear fallout and half of the world has been destroyed and we need your knowledge to make the nukes again or at least a few missiles.")
         result = input("Do you accept this task? Yes or No")
 
     if (result == "Yes") or (result == "yes"):
@@ -42,7 +46,7 @@ def main():
         print("So you continue your journey and you are ambushed by enemy forces.")
         answer = input("How do you want to defend? Run or Fight").lower()
         if answer == "fight":
-            answer == input("How do you fight the enimies make this choice wisley there is only 1 right answer any others and you will die.").lower()
+            answer = input("How do you fight the enemies? Make this choice wisely there is only 1 right answer any others and you will die.").lower()
             if answer == "gun":
                 handle(True)
             else:
@@ -56,6 +60,44 @@ def main():
     answer = input("You have reached the research center now you need to build the missile before the enemy launches. Do you want to start building?").lower()
     if answer == "yes":
         print("You have chosen to start building the missile.")
-        
+
+        RED = "\033[91m"
+        GREEN = "\033[92m"
+        RESET = "\033[0m"
+
+        def reaction():
+            print("The enemy is about to launch their own missile you need to click launch at exactly the right moment")
+            print("Wait for green and press 'c' within 500ms otherwise you will die")
+            time.sleep(7.5)
+            print(f"{RED}RED{RESET}")
+            time.sleep(random.uniform(2, 5)) 
+            print(f"{GREEN}GREEN{RESET}")
+
+            start_time = time.time()
+            timeout = 0.5
+
+            def check_key():
+                while time.time() - start_time < timeout:
+                    if msvcrt.kbhit():
+                        key = msvcrt.getch().decode('utf-8').lower()
+                        if key == 'c':
+                            print("I guess you did it.")
+                            return True
+                print("Sorry to say it but you were too slow")
+                return False
+
+            if check_key():
+                handle(True)
+            else:
+                print("You have died due to slow reflexes.")
+                handle("Dead")
+
+        reaction()
+        missile_building()
+    elif answer == "no":
+        print("You BETRAY us after all of this so you deserve this death")
+        handle("Dead")
+    else:
+        handle("Fail")
 
 main()
